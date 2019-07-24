7b24dc9237e4749517a2d31702087fbfbf790586
eopnetto <Buh$022108>
2019-07-23 21:28:11 -0300

    Panel component and scope

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-    <div>
-        <h1>{{ titulo }}</h1>
+    <div class="corpo">
 
-        <ul>
-            <li v-for="foto of fotos">
-                <img :src="foto.url" :alt="foto.titulo">
+        <h1 class="centralizado">{{ titulo }}</h1>
+
+        <ul class="lista-fotos">
+
+            <li class="lista-fotos-item" v-for="foto in fotos">
+
+                <meu-painel :titulo="foto.titulo">
+                    <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+                </meu-painel>
+
             </li>
         </ul>
 
@@ -12,8 +18,12 @@
 </template>
 
 <script>
-    export default {
+    import Painel from './components/shared/painel/Painel';
 
+    export default {
+        components: {
+            'meu-painel': Painel,
+        },
         data() {
 
             return {
@@ -33,4 +43,27 @@
 </script>
 
 <style>
+
+    .centralizado {
+        text-align: center;
+    }
+
+    .corpo {
+        font-family: Helvetica, sans-serif;
+        margin: 0 auto;
+        width: 96%;
+    }
+
+    .lista-fotos {
+        list-style: none;
+    }
+
+    .lista-fotos .lista-fotos-item {
+        display: inline-block;
+    }
+
+    .imagem-responsiva {
+        width: 100%;
+    }
+
 </style>
